b42a8843ef6e13a872e963972c7da073ad012a8b
Colton stephens <stephens.colton@icloud.com>
2020-03-09 21:35:59 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,11 +1,21 @@
-def my_hash
+def my_hash #[sports_team => houston rockets, team_color => red]
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  my_hash = Hash.new
+  my_hash["sports_team"] = "houston_rockets" 
+  my_hash["team_color"] = "red" 
+  # my_hash = { "sports_team" => "houston rockets", "team_color" => "red" }
+  return my_hash
 end
 
 
-def shipping_manifest
+def shipping_manifest 
+  the_manifest ={
+  "whale bone corsets" => 5,
+  "porcelain vases" => 2, 
+  "oil paintings" => 3
+}
+
+
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
@@ -15,10 +25,13 @@ end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+  "whale bone corsets" => 5, 
+  "porcelain vases" => 2, 
+  "oil paintings" => 3
+  }
+   shipping_manifest["oil paintings"]
+  
+
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
@@ -26,10 +39,11 @@ end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+  "whale bone corsets" => 5, 
+  "porcelain vases" => 2, 
+  "oil paintings" => 3,
+  "muskets" => 2,
+"gun powder" => 4 }
   # add 2 muskets to the shipping_manifest hash below
 
 
